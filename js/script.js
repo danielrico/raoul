e847edecfe64734ac1f637053114e1e1909a5b25
danielrico <maildanielrico@gmail.com>
2015-04-01 20:13:42 +0200

    if we don't want to / can't use an icon library, we can use simple text. Or we can use both : the text is used if no icon has been selected.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
 jQuery(document).ready(function ($) {
 
-    $('body').bind('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta /120 > 0) {
+  
+  // Slide bookmarks
+  
+  $('body').bind('mousewheel', function(e){
+    if(e.originalEvent.wheelDelta /120 > 0) {
+      $("#bm").animate({
+        top: "80%"
+      }, "500" );
+      $("#overlay")
+        .removeClass("overlay_active")
+        .fadeOut("slow");
+      $("#time").delay(200).fadeTo( 100, 800);
+    }
+    else{
+      $("#overlay")
+        .fadeIn("slow")
+        .addClass("overlay_active").click(function() {
+          $("#time").fadeTo( 1000, 100);
           $("#bm").animate({
               top: "80%"
             }, "500" );
-          $("#overlay")
-            .removeClass("overlay_active")
+          $(this)
+            .removeClass("active")
             .fadeOut("slow");
-          $("#time").delay(200).fadeTo( 100, 800);
-        }
-        else{
-          $("#overlay")
-            .fadeIn("slow")
-            .addClass("overlay_active").click(function() {
-              $("#time").fadeTo( 1000, 100);
-              $("#bm").animate({
-                  top: "80%"
-                }, "500" );
-              $(this)
-                .removeClass("active")
-                .fadeOut("slow");
-            });
-          $("#time").fadeTo( 400, 0.33);
-          $("#bm").animate({
-              top: "5%"
-            }, "300" );
-        }
-    });
+        });
+      $("#time").fadeTo( 400, 0.33);
+      $("#bm").animate({
+          top: "5%"
+        }, "300" );
+    }
+  });
+  
+  // Letter bookmarks
+  
+  var text = 0
+  
+  $("#bm li").each(function() {
+    text = $( this ).find("span").text();
+    $( this ).find("em").text(text.slice(0,2));
+  });
+
+
   
 
 });
